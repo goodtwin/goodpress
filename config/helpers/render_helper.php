c873487433862043eb52c94902b0554623b09549
Greg Thompson <greg@good-twin.com>
2014-01-07 08:28:36 -0600

    removing all things haml. simplifying render_helper

--- config/helpers/render_helper.php
+++ config/helpers/render_helper.php
@@ -8,46 +8,20 @@ class RenderHelper {
   }
 
   function render_template($name, $locals = array()) {
-    $valid_filenames = array("$name.html.haml", "$name.haml", "$name.html.php", "$name.php");
+    $valid_filenames = array("$name.html.php", "$name.php");
     foreach ($valid_filenames as $filename) {
       $path = Wordless::join_paths(Wordless::theme_views_path(), $filename);
       if (is_file($path)) {
         $template_path = $path;
-        $format = array_pop(explode('.', $path));
         break;
       }
     }
 
     if (!isset($template_path)) {
-      render_error("Template missing", "<strong>Ouch!!</strong> It seems that <code>$name.html.haml</code> or <code>$name.html.php</code> doesn't exist!");
+      render_error("Template missing", "<strong>Ouch!!</strong> It seems that <code>$name.html.php</code> doesn't exist!");
     }
 
-    extract($locals);
-
-    switch ($format) {
-      case 'haml':
-        $tmp_dir = Wordless::theme_temp_path();
-
-        if (!file_exists($tmp_dir)) {
-          mkdir($tmp_dir, 0760);
-        }
-
-        if (!is_writable($tmp_dir)) {
-          chmod($tmp_dir, 0760);
-        }
-
-        if (is_writable($tmp_dir)) {
-          $haml = new HamlParser(array('style' => 'expanded', 'ugly' => false/*, 'helperFile' => dirname(__FILE__).'/../ThemeHamlHelpers.php'*/));
-          include $haml->parse($template_path, $tmp_dir);
-        } else {
-          render_error("Temp dir not writable", "<strong>Ouch!!</strong> It seems that the <code>$tmp_dir</code> directory is not writable by the server! Go fix it!");
-        }
-        break;
-      case 'php':
-        include $template_path;
-        break;
-    }
-
+    include $template_path;
   }
 
   function get_partial_content($name, $locals = array()) {
